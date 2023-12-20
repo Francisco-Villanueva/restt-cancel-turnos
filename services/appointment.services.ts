f37ae7ea6347582d9544f03fc99896f7eeabfb4d
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-12-20 17:26:54 -0300

    added api key

--- services/appointment.services.ts
+++ services/appointment.services.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_KEY = process.env.NEXT_API_KEY;
 
 export class AppointmentServices {
   static async getTurnoById(turnoId: string) {
-    return await axios.get(`${API_URL}/appointment/${turnoId}`);
+    return await axios.get(
+      `${API_URL}/appointment/${turnoId}/?API_KEY=${API_KEY}`
+    );
   }
   static async delete(turnoId: string) {
-    return await axios.delete(`${API_URL}/appointment/${turnoId}`);
+    return await axios.delete(
+      `${API_URL}/appointment/${turnoId}/?API_KEY=${API_KEY}`
+    );
   }
 }
--- services/barber.services.ts
+++ services/barber.services.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_KEY = process.env.NEXT_API_KEY;
 
 export class BarberServices {
   static async getAllBarbers() {
-    return await axios.get(`${API_URL}/barbers`);
+    return await axios.get(`${API_URL}/barbers/?API_KEY=${API_KEY}`);
   }
   static async getBarberById(barberId: number) {
-    return await axios.get(`${API_URL}/barbers/${barberId}`);
+    return await axios.get(
+      `${API_URL}/barbers/${barberId}/?API_KEY=${API_KEY}`
+    );
   }
 }
